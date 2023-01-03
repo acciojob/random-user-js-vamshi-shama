e546b27e1dccac1220a57221265badfc5c20a486
vamshi <vamshis2017@gmail.com>
2023-01-03 23:22:11 +0530

    .

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+const getUser = document.getElementById('getUser');
+getUser.addEventListener('click', displayDetails);
+
+function displayDetails() {
+    fetch('https://randomuser.me/api/')
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        data.array.forEach(element => {
+            const markup = `<li>${element.name}${element.picture}</li>`;
+
+            document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
+        });
+    })
+}
