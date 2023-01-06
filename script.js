5456af8cb5b08a98011a9485723873ae595861d4
vamshi <vamshis2017@gmail.com>
2023-01-06 22:50:34 +0530

    .

--- script.js
+++ script.js
@@ -2,8 +2,30 @@
 const getUser = document.getElementById('getUser');
 getUser.addEventListener('click', displayDetails);
 
-function displayDetails() {
-    fetch('https://randomuser.me/api/')
-        .then(res => res.json())
-        .then(data => console.log(data))
+async function displayDetails () {
+    const response =  await fetch("https://randomuser.me/api/");
+
+    var data = await response.json();
+    console.log(data)
+    // if (response) {
+    //     hideloader();
+    // }
+    show(data);
+}
+
+const show = (data) => {
+    let tab = 
+            `<tr>
+                <th>Name</th>
+                <th>Picture</th>
+            </tr>`;
+
+    for (let r of data.results) {
+        tab += `<tr>
+            <td>${r.name}</td>
+            <td>${r.picture}</td>
+        </tr>`;
+    }
+    
+    document.getElementById('userDetails').innerHTML = tab;
 }
